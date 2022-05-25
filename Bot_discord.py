6bcd33cd1b3e146c7d7fec81218cb9f006586839
Ewan Questier <ewan.questier@gmail.com>
2022-05-25 09:57:55 +0100

    test

--- Bot_discord.py
+++ Bot_discord.py
@@ -8,7 +8,6 @@ client = commands.Bot(command_prefix="!")
 @client.command()
 async def coucou(ctx):
     await ctx.send("COUCOU !!!")
-
 @client.command()
 async def super_coucou(ctx, arg):
     await ctx.send(arg)
@@ -21,12 +20,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     # c'est le channel où les réponses du bot seront
-<<<<<<< HEAD
     Help_channel = client.get_channel(978272410069516359)
-=======
-    # changer '978272464683540530' en fonction de son channel
-    Help_channel = client.get_channel(978583666517241906)
->>>>>>> vito
 
     # Si on écrit $help
     if message.channel == Help_channel and message.content.startswith('$help'):
@@ -38,13 +32,40 @@ async def on_message(message):
         # le bot supprimera les 3 derniers messages
         await message.channel.purge(limit=3)
 
-    if message.content.startswith('$dm'): # $dm @lapersonne msg
+    if message.content.startswith('$dm'): # $dm @lapersonnequelonveut msg
         user = message.mentions[0]
         strs = message.content.split(" ")
-        await user.send(' '.join(strs[2:]))
+        await user.send(strs[2:])
 
     await client.process_commands(message)
 
+class Node:
+
+    def __init__(self, question, keyword, list_child_node):
+        self.question = question
+        self.keyword = keyword
+        self.list_child_node = list_child_node
+    
+    def user_response(self):
+        print(self.question)
+        txt = input()
+        for child in self.list_child_node:
+            if child.keyword in txt:
+                child.user_response()
+
+first_node = Node("Comment puis-je vous aider ?", "help", [Node("Sur quel sujet ?", "cours", []), Node("Sur quel domaine ?", "fichier", [])])
+
+Node.user_response(first_node)
+
+# default_intents = discord.Intents.default()
+# default_intents.members = True
+# client = discord.Client(intents=default_intents)
+
+# @client.event
+# async def on_member_join(member):
+#     arrival_channel = client.get_channel(978295746325545010)
+#     member.send(member.display_name,"a rejoint le serveur !")
+    
+
 # commande permettant de connecter son bot au serveur Discord
-# changer par rapport à son bot OTc4MjI5MzQ1MzU1MTk4NDY0.GQQW_r.r8xPYJiyOwtskNhupCanrq5FDGKNmY8fbLVgQI
-client.run("OTc4MjI5MzQ1MzU1MTk4NDY0.GmtTj-.m-DZcC0V-XkPTCNoTMNgyFlxQjn2XbhPXguazU")
+client.run("OTc4MjI5MzQ1MzU1MTk4NDY0.GQQW_r.r8xPYJiyOwtskNhupCanrq5FDGKNmY8fbLVgQI")
