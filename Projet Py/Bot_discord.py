759d3a83b08954904b9cb3cfeb13b77e45ec553e
Loïc Jin <loicjin@MacBook-Pro-de-Loic.local>
2022-05-25 11:04:55 +0200

    test

--- Projet Py/Bot_discord.py
+++ Projet Py/Bot_discord.py
@@ -1,18 +1,36 @@
 from email.policy import default
-import discord 
 
 from discord.ext import commands
+from Arbre import *
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="$")
 
 @client.command()
 async def coucou(ctx):
     await ctx.send("COUCOU !!!")
 
 @client.command()
-async def super_coucou(ctx, arg):
-    await ctx.send(arg)
-    
+async def bot(ctx):
+    await ctx.send("Bienvenue, voici la liste des commandes possible sur le serveur ! \n"+
+    "$help : envoie une liste de question posée par le bot à l'utilisateur \n"+
+    "$help_tips : envoie un exemple d'utilisation sur la commande $help \n"+
+    "$admin : envoie un dm à un admin pour répondre au question de l'utilisateur\n"+
+    "$bonus : envoie un lien bonus pour l'utilisateur \n"+
+    "$cours : envoie la liste de cours proposé \n"+
+    "$tuto : envoie la liste de cours en vidéo proposé par le bot")
+
+# @client.command()
+# async def help(ctx):
+#     await ctx.send(node1)
+
+@client.command()
+async def bonus(ctx):
+    await ctx.send("<https://www.youtube.com/watch?v=iik25wqIuFo>")
+# @client.command()
+# async def super_coucou(ctx, arg):
+#     await ctx.send(arg)
+
+
 @client.event
 async def on_message(message):
     message.content = message.content.lower()
@@ -21,7 +39,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     # c'est le channel où les réponses du bot seront
-    Help_channel = client.get_channel(978272464683540530)
+    # changer '978272464683540530' en fonction de son channel
+    Help_channel = client.get_channel(978234499177529364)
 
     # Si on écrit $help
     if message.channel == Help_channel and message.content.startswith('$help'):
@@ -31,24 +50,15 @@ async def on_message(message):
     # Si on écrit del
     if message.content == "del":
         # le bot supprimera les 3 derniers messages
-        await message.channel.purge(limit=3)
+        await message.channel.purge(limit=7)
 
-    if message.content.startswith('$dm'): # $dm @lapersonnequelonveut msg
+    if message.content.startswith('$dm'): # $dm @lapersonne msg
         user = message.mentions[0]
         strs = message.content.split(" ")
-        await user.send(strs[2:])
+        await user.send(' '.join(strs[2:]))
 
     await client.process_commands(message)
 
-# default_intents = discord.Intents.default()
-# default_intents.members = True
-# client = discord.Client(intents=default_intents)
-
-# @client.event
-# async def on_member_join(member):
-#     arrival_channel = client.get_channel(978295746325545010)
-#     member.send(member.display_name,"a rejoint le serveur !")
-    
-
 # commande permettant de connecter son bot au serveur Discord
-client.run("OTc4MjI5MzQ1MzU1MTk4NDY0.GQQW_r.r8xPYJiyOwtskNhupCanrq5FDGKNmY8fbLVgQI")
+# changer par rapport à son bot OTc4MjI5MzQ1MzU1MTk4NDY0.GQQW_r.r8xPYJiyOwtskNhupCanrq5FDGKNmY8fbLVgQI
+client.run("OTc4MjI5MzE5ODk5OTQ3MDA4.G2Nqaw.Nop0HfAjM4cAguagp7LdRiMmGSYVdKExyQjHA4")
