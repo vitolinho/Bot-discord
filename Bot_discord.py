d80d9d255a2193587c63e63e85be0435561413be
Yvelchrome <yvelchrome@gmail.com>
2022-06-01 11:57:58 +0200

    feat: /help et autres commandes

--- Bot_discord.py
+++ Bot_discord.py
@@ -1,46 +1,88 @@
-from email.policy import default
-import discord 
-
+import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
+# Associe le prefix pour faire des commandes à /
+client = commands.Bot(command_prefix="/")
+# Enlève la commande help intégrée de base
+client.remove_command("help")
 
-@client.command()
-async def coucou(ctx):
-    await ctx.send("COUCOU !!!")
-
-@client.command()
-async def super_coucou(ctx, arg):
-    await ctx.send(arg)
-    
+# Affiche le message dans le terminal pour dire que le bot est fonctionnel
 @client.event
-async def on_message(message):
-    message.content = message.content.lower()
+async def on_ready():
+    print('Bot is ready.')
 
-    # éviter les messages à l'infini
-    if message.author == client.user:
-        return
-    # c'est le channel où les réponses du bot seront
-    # changer '978272464683540530' en fonction de son channel
-    Help_channel = client.get_channel(978272464683540530)
+# Commande help pour des informations sur les commandes du bot
+@client.group(invoke_without_command=True)
+async def help(ctx):
+    embed = discord.Embed(title = "Commandes", description = "Utilise /help <commande> pour plus de détails sur une commande")
 
-    # Si on écrit $help
-    if message.channel == Help_channel and message.content.startswith('$help'):
-        # le bot nous dira Bonjour!
-        await Help_channel.send('Bonjour !')
+    embed.add_field(name='Aide', value='aide')
+    embed.add_field(name='Liste des :', value='cours , tuto')
+    embed.add_field(name='DM', value='dm')
+    embed.add_field(name='Admin', value='clear , stop')
 
-    # Si on écrit del
-    if message.content == "del":
-        # le bot supprimera les 3 derniers messages
-        await message.channel.purge(limit=3)
+    await ctx.send(embed=embed)
 
-    if message.content.startswith('$dm'): # $dm @lapersonnequelonveut msg
-        user = message.mentions[0]
-        strs = message.content.split(" ")
-        await user.send(strs[2:])
+@help.command()
+async def aide(ctx):
+    embed = discord.Embed(title = "Aide", description = "Envoie une liste de questions posées par le bot à l'utilisateur")
+    embed.add_field(name='**Syntaxe**', value='``/aide``', inline=False)
 
-    await client.process_commands(message)
+    await ctx.send(embed=embed)
 
-# commande permettant de connecter son bot au serveur Discord
-# changer par rapport à son bot OTc4MjI5MzQ1MzU1MTk4NDY0.GQQW_r.r8xPYJiyOwtskNhupCanrq5FDGKNmY8fbLVgQI
-client.run("OTc4MjI5MzQ1MzU1MTk4NDY0.GQQW_r.r8xPYJiyOwtskNhupCanrq5FDGKNmY8fbLVgQI")
+@help.command()
+async def cours(ctx):
+    embed = discord.Embed(title = "Cours", description = "Envoie la liste des cours écris proposés par le bot")
+    embed.add_field(name='**Syntaxe**', value='``/cours``', inline=False)
+
+    await ctx.send(embed=embed)
+
+@help.command()
+async def tuto(ctx):
+    embed = discord.Embed(title = "Tuto", description = "Envoie la liste des cours en vidéo proposés par le bot")
+    embed.add_field(name='**Syntaxe**', value='``/tuto``', inline=False)
+
+    await ctx.send(embed=embed)
+
+@help.command()
+async def dm(ctx):
+    embed = discord.Embed(title = "DM", description = "Envoie un message privé à la personne tag")
+    embed.add_field(name='**Syntaxe**', value='``/dm @user message``', inline=False)
+
+    await ctx.send(embed=embed)
+
+@help.command()
+async def clear(ctx):
+    embed = discord.Embed(title = "Clear **(Commande admin)**", description = "Efface les 100 messages précédent du salon")
+    embed.add_field(name='**Syntaxe**', value='``/clear``', inline=False)
+
+    await ctx.send(embed=embed)
+
+@help.command()
+async def stop(ctx):
+    embed = discord.Embed(title = "Stop **(Commande admin)**", description = "Stop le bot")
+    embed.add_field(name='**Syntaxe**', value='``/stop``', inline=False)
+
+    await ctx.send(embed=embed)
+
+# Permet d'envoyer un dm à une personne
+@client.command()
+async def dm(ctx, user:discord.User, *, message=None):
+    message = message
+    embed = discord.Embed(title=message)
+    await user.send(embed=embed)
+
+# Enlève les 100 précédent messages du chat
+@client.command()
+@commands.is_owner()
+async def clear(ctx, amount=100):
+    await ctx.channel.purge(limit=amount)
+
+# Stop le bot
+@client.command()
+@commands.is_owner()
+async def stop(ctx):
+    exit()
+
+# Lance le bot 
+client.run("bot_token")
